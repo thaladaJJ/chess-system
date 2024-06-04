5edf127ba5d1346b9bdb42145763f7d8fa80f3c2
Thales Euler <thaleslevs@gmail.com>
2024-06-04 20:20:03 -0300

    board & piece

--- application/Main.java
+++ application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
@@ -7,7 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         Position pos = new Position(5, 7);
-        System.out.println(pos);
+        System.out.println (pos);
+
+        Board board = new Board (8, 8);
+
+
 
     }
 
