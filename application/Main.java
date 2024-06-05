f3b8cdd664f5f3de9d973669b083314a335bb828
Thales Euler <thaleslevs@gmail.com>
2024-06-04 21:06:04 -0300

    interface de jogo / tabuleiro preparada

--- application/Main.java
+++ application/Main.java
@@ -2,17 +2,14 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(5, 7);
-        System.out.println (pos);
-
-        Board board = new Board (8, 8);
-
-
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard (chessMatch.getPieces());
 
     }
 
